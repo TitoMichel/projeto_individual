41d2f13534105e4eb9814350e052bed9ead96aca
TitoMichel <michel.mascarenhas@sptech.school>
2025-05-12 22:49:53 -0300

    Atualizando tela de Fale Conosco+ Scripts do BD

--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -16,6 +16,82 @@ email varchar(100),
 senha varchar(100),
 cpf char(11));
 
+CREATE TABLE jogos (
+    id_jogo INT AUTO_INCREMENT PRIMARY KEY,
+    adversario VARCHAR(50) NOT NULL,
+    data_jogo DATETIME NOT NULL,
+    local VARCHAR(100) NOT NULL,
+    rodada VARCHAR(20),
+    estadio VARCHAR(50),
+    campeonato VARCHAR(30) DEFAULT 'Série C'
+);
+
+INSERT INTO jogos (adversario, data_jogo, local, rodada, estadio, campeonato) 
+VALUES ('RB Bragantino', '2025-01-19 21:00:00', 'São Bernardo do Campo', '2ª Rodada', 'Estádio 1º de Maio', 'Paulista');
+
+INSERT INTO jogos (adversario, data_jogo, local, rodada, estadio, campeonato) 
+VALUES ('Mirassol', '2025-01-23 18:30:00', 'São Bernardo do Campo', '3ª Rodada', 'Estádio 1º de Maio', 'Paulista');
+
+INSERT INTO jogos (adversario, data_jogo, local, rodada, estadio, campeonato) 
+VALUES ('Santos FC', '2025-01-29 18:30:00', 'São Bernardo do Campo', '5ª Rodada', 'Estádio 1º de Maio', 'Paulista');
+
+INSERT INTO jogos (adversario, data_jogo, local, rodada, estadio, campeonato) 
+VALUES ('Grêmio Novorizontino', '2025-02-06 19:00:00', 'São Bernardo do Campo', '7ª Rodada', 'Estádio 1º de Maio', 'Paulista');
+
+INSERT INTO jogos (adversario, data_jogo, local, rodada, estadio, campeonato) 
+VALUES ('Guarani', '2025-02-16 17:00:00', 'São Bernardo do Campo', '10ª Rodada', 'Estádio 1º de Maio', 'Paulista');
+
+INSERT INTO jogos (adversario, data_jogo, local, rodada, estadio, campeonato) 
+VALUES ('São Paulo', '2025-02-23 16:00:00', 'São Bernardo do Campo', '12ª Rodada', 'Estádio 1º de Maio', 'Paulista');
+
+INSERT INTO jogos (adversario, data_jogo, local, rodada, estadio, campeonato) 
+VALUES ('Palmeiras', '2025-03-02 18:00:00', 'São Bernardo do Campo', '13ª Rodada', 'Estádio 1º de Maio', 'Paulista');
+
+-- Jogos da série C:
+
+INSERT INTO jogos (adversario, data_jogo, local, rodada, estadio, campeonato) 
+VALUES ('ABC', '2025-04-12 16:00:00', 'São Bernardo do Campo', '1ª Rodada', 'Estádio 1º de Maio', 'Série C');
+
+INSERT INTO jogos (adversario, data_jogo, local, rodada, estadio, campeonato) 
+VALUES ('Londrina', '2025-05-03 19:00:00', 'São Bernardo do Campo', '4ª Rodada', 'Estádio 1º de Maio', 'Série C');
+
+INSERT INTO jogos (adversario, data_jogo, local, rodada, estadio, campeonato) 
+VALUES ('Caxias do Sul', '2025-05-17 19:00:00', 'São Bernardo do Campo', '5ª Rodada', 'Estádio 1º de Maio', 'Série C');
+
+INSERT INTO jogos (adversario, data_jogo, local, rodada, estadio, campeonato) 
+VALUES ('CSA', '2025-05-31 19:00:00', 'São Bernardo do Campo', '7ª Rodada', 'Estádio 1º de Maio', 'Série C');
+
+INSERT INTO jogos (adversario, data_jogo, local, rodada, estadio, campeonato) 
+VALUES ('Anápolis', '2025-06-28 19:00:00', 'São Bernardo do Campo', '9ª Rodada', 'Estádio 1º de Maio', 'Série C');
+
+INSERT INTO jogos (adversario, data_jogo, local, rodada, estadio, campeonato) 
+VALUES ('Ponte Preta', '2025-07-12 19:00:00', 'São Bernardo do Campo', '11ª Rodada', 'Estádio 1º de Maio', 'Série C');
+
+INSERT INTO jogos (adversario, data_jogo, local, rodada, estadio, campeonato) 
+VALUES ('Confiança', '2025-07-26 19:00:00', 'São Bernardo do Campo', '13ª Rodada', 'Estádio 1º de Maio', 'Série C');
+
+INSERT INTO jogos (adversario, data_jogo, local, rodada, estadio, campeonato) 
+VALUES ('Tombense', '2025-08-02 19:00:00', 'São Bernardo do Campo', '15ª Rodada', 'Estádio 1º de Maio', 'Série C');
+
+INSERT INTO jogos (adversario, data_jogo, local, rodada, estadio, campeonato) 
+VALUES ('Figueirense', '2025-08-16 19:00:00', 'São Bernardo do Campo', '17ª Rodada', 'Estádio 1º de Maio', 'Série C');
+
+INSERT INTO jogos (adversario, data_jogo, local, rodada, estadio, campeonato) 
+VALUES ('Náutico', '2025-08-23 19:00:00', 'São Bernardo do Campo', '18ª Rodada', 'Estádio 1º de Maio', 'Série C');
+
+
+
+CREATE TABLE confirmados (
+id_confirmacao INT AUTO_INCREMENT PRIMARY KEY,
+id_socio INT NOT NULL,
+id_jogo INT NOT NULL,
+status ENUM('Confirmado', 'Não vai', 'Pendente') DEFAULT 'Pendente',
+data_confirmacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+FOREIGN KEY (id_socio) REFERENCES socios(id),
+FOREIGN KEY (id_jogo) REFERENCES jogos(id_jogo),
+UNIQUE KEY (id_socio, id_jogo) 
+);
+
 
 
 /* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
