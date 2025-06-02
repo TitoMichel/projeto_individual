3e69992fb72722c93d88b53dc32468b1f5f7880e
TitoMichel <michel.mascarenhas@sptech.school>
2025-06-02 15:01:45 -0300

    Adicionando slide+modificando fotos

--- public/js/script.js
+++ public/js/script.js
@@ -2,27 +2,31 @@ var i =0;
 var txt1 = `Movimento Popular febre Amarela`
 var txt2 = `A Primeira barra brava do São Bernardo Futebol Clube. `
 
-var speed = 50;
+var velocidade = 50;
 
 function escrever(){
     if(i<txt1.length){
         p1.innerHTML += txt1.charAt(i);
         i++;
-        setTimeout(escrever,speed)
-        if(i>19){
-            
-        }
+        setTimeout(escrever,velocidade)
+        
     } else if(i==txt1.length){
         p1.innerHTML+=`<br> `;
-        p1.innerHTML += txt2.charAt(i-txt2.length)
+        p1.innerHTML += txt2.charAt(i-txt1.length)
         i++
-        setTimeout(escrever,speed)
+        setTimeout(escrever,velocidade)
     }
-    
+
     else {
-        p1.innerHTML += txt2.charAt(i-txt2.length)
+        p1.innerHTML += txt2.charAt(i-txt1.length)
         i++
-        setTimeout(escrever,speed)
+        if(i==txt1.length+txt2.length){
+            return;
+        }
+        setTimeout(escrever,velocidade)
+       
+        
+        
     }    
 }
 
